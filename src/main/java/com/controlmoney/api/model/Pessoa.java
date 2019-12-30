91298433089ddcce6c836754bafa3b8bce1cc5aa
Gil Brandao <gcbrandao@gmail.com>
2019-12-30 16:41:06 -0300

    adicionando metodo de resumo de lancamentos para retornar informaçoes mais resumidas

--- src/main/java/com/controlmoney/api/model/Pessoa.java
+++ src/main/java/com/controlmoney/api/model/Pessoa.java
@@ -1,22 +1,15 @@
 package com.controlmoney.api.model;
 
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "pessoa")
 public class Pessoa {
-	
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
@@ -30,10 +23,7 @@ public class Pessoa {
 	
 	@Embedded
 	private Endereco endereco;
-	
-	
 
-	
 	
 	public Endereco getEndereco() {
 		return endereco;
--- src/main/java/com/controlmoney/api/resource/LancamentoResource.java
+++ src/main/java/com/controlmoney/api/resource/LancamentoResource.java
@@ -5,6 +5,7 @@ import com.controlmoney.api.exceptionhandler.AlgamoneyExceptionHandler.Erro;
 import com.controlmoney.api.model.Lancamento;
 import com.controlmoney.api.repository.LancamentoRepository;
 import com.controlmoney.api.repository.filter.LancamentoFilter;
+import com.controlmoney.api.repository.projection.ResumoLancamento;
 import com.controlmoney.api.service.LancamentoService;
 import com.controlmoney.api.service.ecception.PessoaInexistenteOuInativaException;
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -46,6 +47,13 @@ public class LancamentoResource {
     public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
 
         return lancamentoRepository.filtrar(lancamentoFilter, pageable);
+    }
+
+    @GetMapping(params = "resumo")
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
+    public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
+
+        return lancamentoRepository.resumir(lancamentoFilter, pageable);
     }
 
     @GetMapping("/{codigo}")
